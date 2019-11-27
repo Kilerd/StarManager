812ce6ceda725ba18e28b1815ae7a3073c838493
Kilerd Chan <blove694@gmail.com>
2019-11-27 15:31:04 +0800

    feat: show archived repo.

--- src/Components/RepoItem/index.jsx
+++ src/Components/RepoItem/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 import './style.scss';
+
+
+const TagSpan = styled.span`
+  color: grey;
+  border: 1px solid grey;
+  font-size: 60%;
+  border-radius: 3px;
+  margin-left: 5px;
+`;
 
 export default function RepoItem(props) {
   const { repo } = props;
@@ -11,10 +21,11 @@ export default function RepoItem(props) {
   const language = repo.primaryLanguage ? (
     <span style={{ color: repo.primaryLanguage.color }}>{repo.primaryLanguage.name}</span>) : '';
   const license = repo.licenseInfo ? (<span>{repo.licenseInfo.name}</span>) : '';
+  const isArchive = repo.isArchived ? <TagSpan>Archived</TagSpan> : '';
   return (
     <div className="repo-item" onClick={onClick} onKeyPress={onClick} role="button" tabIndex="0">
       <div className="name">
-        {repo.nameWithOwner}
+        {repo.nameWithOwner} {isArchive}
       </div>
       <div className="description">
         {repo.description}
